b1b7eca0c04582e73dd44683efcd50ace32c58b9
apurslow <aaronp26@uw.edu>
2023-09-24 15:44:12 -0700

    did a thing

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
 import { OrbitControls } from "@react-three/drei";
 
+import { Suspense } from "react";
+import { Loader } from "./LoadingScreen";
+import { Crash } from "./Crash";
+
 export const Experience = () => {
   return (
     <>
+      <Suspense fallback={<Loader />}></Suspense>
       <OrbitControls />
+      <ambientLight intensity={1} />
       <mesh>
-        <boxGeometry />
+       
         <meshNormalMaterial />
+        <Crash  />
       </mesh>
     </>
   );
